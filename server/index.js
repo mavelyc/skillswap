c4175115278342bb442537b46a5e2c97a32502f8
mavelyc <cmavely@gmail.com>
2019-10-26 02:09:59 -0400

    firebase working

--- server/index.js
+++ server/index.js
@@ -2,17 +2,39 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 3000
+const admin = require('firebase-admin');
 
-app.use(bodyParser.json())
-app.use(
-  bodyParser.urlencoded({
-    extended: true,
-  })
-)
+let serviceAccount = require('./serviceAccountKey.json');
 
-app.get('/', (req,res) => {
-    res.send('Hello World');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
 });
+
+let db = admin.firestore();
+
+let docRef = db.collection('users').doc('alovelace');
+
+let setAda = docRef.set({
+  first: 'Ada',
+  last: 'Lovelace',
+  born: 1815
+});
+
+
+
+
+// app.use(bodyParser.json())
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: true,
+//   })
+// )
+
+// app.get('/:user', (req,res) => {
+//     res.send('Hello World');
+// });
+
+
 
 app.listen(port, () => {
     console.log(`App is running on ${port}`);
