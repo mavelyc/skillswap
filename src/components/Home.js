cd0eb492c9525a3901fd60b5caf4e30c95a6d671
mmavely <mgmavely@gmail.com>
2019-10-26 15:20:24 -0400

    helo

--- src/components/Home.js
+++ src/components/Home.js
@@ -10,10 +10,19 @@ const Home = () => {
     <div>
       Home
       <h1>Welcome to SkillSwap</h1>
-        <Login />
-        <Link to="/register">
-            <button>Register</button>
+      <div class="container">
+       <div class="row">
+       <div class="col-sm">
+          <Link to="/register">
+        <button type="button" class="btn btn-outline-primary">
+          Register
+        </button>
         </Link>
+        </div>
+        <div class="col-sm"></div>
+          <Login />
+        </div>
+        </div>
     </div>
   )
 }
