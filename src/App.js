2f86670ba55fc73acef3beed6328735f7229e65e
Tristan Wedderburn <tristan.wedderburn8@gmail.com>
2019-10-26 11:02:14 -0400

    added new routes from main menu Co-authored-by: mgmavely <mgmavely@gmail.com> Co-authored-by: siddik1 <siddik1@mcmaster.ca> Co-authored-by: Christian Mavely <mavelyc@mcmaster.ca>

--- src/App.js
+++ src/App.js
@@ -5,15 +5,12 @@ import {
   Route,
   Link
 } from "react-router-dom";
-import logo from './logo.svg';
-import './App.css';
 import Root from './components/Root';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
         <Root />
       </header>
     </div>
--- src/components/MainMenu.js
+++ src/components/MainMenu.js
@@ -5,10 +5,13 @@ import {
   Route,
   Link
 } from "react-router-dom";
+import '../App.css';
+import logo from '../logo.svg';
 
 const MainMenu = () => {
     return(
       <div>
+        <img src={logo} className="App-logo" alt="logo" />
         <h1>Welcome to SkillSwap</h1>
         <Link to="/login">
           <button>Login</button>
